8efab90dd08d0e86053b1f04d8a6d56fea6d2960
midspooj <142264378+midspooj@users.noreply.github.com>
2023-12-08 20:48:42 +0000

    trying to push the updated naive bayes code

--- code/naive-bayes.py
+++ code/naive-bayes.py
@@ -1,5 +1,6 @@
 import json
 import pandas as pd
+import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
@@ -21,7 +22,6 @@ with open(test_path, 'r') as test_file:
 train_df = pd.DataFrame(train_data)
 test_df = pd.DataFrame(test_data)
 
-
 # Bin the 'Score' values into discrete classes
 num_bins = 5  # You can adjust the number of bins based on your preference
 train_df['Score'] = pd.cut(train_df['Score'], bins=num_bins, labels=False)
@@ -32,8 +32,8 @@ train_df['Score'] = train_df['Score'].astype(str)
 test_df['Score'] = test_df['Score'].astype(str)
 
 # Concatenate question and answer for both train and test
-X_train = train_df['Question'] + ' ' + train_df['Score'] + ' ' + train_df['Answer']
-X_test = test_df['Question'] + ' ' + test_df['Score'] + ' ' + test_df['Answer']
+X_train = train_df['Question'] + ' ' + train_df['Answer']
+X_test = test_df['Question'] + ' ' + test_df['Answer']
 
 # Use 'Score' as the target variable
 y_train = train_df['Score']
@@ -44,8 +44,23 @@ vectorizer = CountVectorizer()
 X_train_bow = vectorizer.fit_transform(X_train)
 X_test_bow = vectorizer.transform(X_test)
 
+def find_best_alpha(X_train, y_train, X_test, y_test):
+    best_alpha = 0
+    best_score = 0
+    for alpha in np.arange(0.1, 1.1, 0.1):
+        model = MultinomialNB(alpha=alpha)
+        model.fit(X_train, y_train)
+        score = model.score(X_test, y_test)
+        if score > best_score:
+            best_alpha = alpha
+            best_score = score
+    print("Best alpha: {}".format(best_alpha))
+    print("Best score: {}".format(best_score))
+    return best_alpha
+
 # Train a naive Bayes classifier
-classifier = MultinomialNB()
+best_alpha = find_best_alpha(X_train_bow, y_train, X_test_bow, y_test)
+classifier = MultinomialNB(alpha = best_alpha)
 classifier.fit(X_train_bow, y_train)
 
 # Make predictions on the test set
