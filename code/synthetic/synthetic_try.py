36de1442326916358246909d2e78af94257bb804
midspooj <142264378+midspooj@users.noreply.github.com>
2023-12-12 02:53:41 +0000

    created the arrays for synthetic data

--- code/synthetic/synthetic_try.py
+++ code/synthetic/synthetic_try.py
@@ -1,55 +1,23 @@
-import json
 import pandas as pd
-import numpy as np
-from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import accuracy_score, confusion_matrix, classification_report, roc_auc_score
-from sklearn.metrics import roc_curve, auc
-import matplotlib.pyplot as plt
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics import f1_score
 
-train_path = r'/workspaces/NLPFinalProject/data/train_formatted_output.json'
-test_path = r'/workspaces/NLPFinalProject/data/test_formatted_output.json'
-valid_path = r'/workspaces/NLPFinalProject/data/valid_formatted_output.json'
+# Load the synthetic data from the CSV file
+synthetic_data_path = '/workspaces/NLPFinalProject/code/synthetic/synthetic_data_NB.csv'
+synthetic_data = pd.read_csv(synthetic_data_path)
 
-# Read the data from the JSON files
-with open(train_path, 'r') as train_file:
-    train_data = [json.loads(line) for line in train_file]
+# Split the data into train, test, and validation sets
+train_data, temp_data = train_test_split(synthetic_data, test_size=0.375, random_state=42)
+test_data, val_data = train_test_split(temp_data, test_size=0.52, random_state=42)
 
-with open(test_path, 'r') as test_file:
-    test_data = [json.loads(line) for line in test_file]
+# Define paths for the new JSON files
+train_json_path = '/workspaces/NLPFinalProject/code/synthetic/synthetic_train_data.json'
+test_json_path = '/workspaces/NLPFinalProject/code/synthetic/synthetic_test_data.json'
+val_json_path = '/workspaces/NLPFinalProject/code/synthetic/synthetic_val_data.json'
 
-with open(test_path, 'r') as valid_file:
-    valid_data = [json.loads(line) for line in valid_file]
+# Save the split data to JSON files as arrays
+train_data.to_json(train_json_path, orient='records')
+test_data.to_json(test_json_path, orient='records')
+val_data.to_json(val_json_path, orient='records')
 
-# Create DataFrames from the lists of dictionaries
-train_df = pd.DataFrame(train_data)
-test_df = pd.DataFrame(test_data)
-valid_df = pd.DataFrame(valid_data)
+print("Data split and saved to JSON files as arrays.")
 
-# Combine the two datasets
-final_df = pd.concat([train_df, test_df, valid_df], ignore_index=True)
-
-# Split the combined final_df into train and temp datasets (62.5% train, 37.5% temp)
-train_df, temp_df = train_test_split(final_df, test_size=0.375, random_state=42)
-
-# Further split the temp_df into test and valid datasets (22% test, 19.5% valid)
-test_df, valid_df = train_test_split(temp_df, test_size=0.52, random_state=42)
-
-# Print the sizes of the datasets
-print("Train Dataset Size:", len(train_df))
-print("Test Dataset Size:", len(test_df))
-print("Valid Dataset Size:", len(valid_df))
-
-save_path = '/workspaces/NLPFinalProject/code/synthetic/'
-
-# Save the train_df as a JSON file
-train_df.to_json(save_path + 'train_s.json', orient='records', lines=True)
-
-# Save the test_df as a JSON file
-test_df.to_json(save_path + 'test_s.json', orient='records', lines=True)
-
-# Save the valid_df as a JSON file
-valid_df.to_json(save_path + 'valid_s.json', orient='records', lines=True)
